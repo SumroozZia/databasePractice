706ab566868bac321dbd6cc59a1f7a16ff2709bf
Sumrooz <sumroozzia1@gmail.com>
2022-10-06 15:05:04 +0200

    dataBase is connected

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = 8001;
 
 
-app.listen(port,()=>{
+// Connection to database
+mongoose.connect('mongodb://localhost:27017/test');
+
+
+const databaseConnection = async()=>{
+
+    try {
+        await mongoose.connect('mongodb://localhost:27017/test');
+        console.log("DataBase is connect")
+    } catch (error) {
+        console.log(error)
+        process.exit(1);
+    }
+}
+app.listen(port,async()=>{
     console.log(`Server is running at http://localhost:${port}`)
+   await databaseConnection();
 })
-
-
 app.get("/",(req,res)=>{
     res.send("Wel to Home page")
+})
+
+app.get("/api/products",(req,res)=>{
+    res.send(" Products are here")
 })
